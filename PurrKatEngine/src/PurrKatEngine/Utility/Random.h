79c0053f78c5d4fa61cf98ddba04ef2ab2eac8bc
AdriKat2022 <adrien.schroedel91@gmail.com>
2026-07-22 15:03:28 +0200

    added random utility class

---
+++ PurrKatEngine/src/PurrKatEngine/Utility/Random.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <random>
+
+#define PKE_RANDOM(min, max) ::PurrKatEngine::Random::Float(min, max);
+#define PKE_RANDOM_INT(min, max) ::PurrKatEngine::Random::Int(min, max);
+
+namespace PurrKatEngine
+{
+    class Random
+    {
+    public:
+        static void Init() { s_RandomEngine.seed(std::random_device()()); }
+        
+        static float Float() { return (float)s_Distribution(s_RandomEngine)/(float)std::numeric_limits<uint32_t>::max(); }
+        static float Float(float min, float max) { return std::lerp(min, max, Float()); }
+        
+        static int Int(int max) { return (int)std::floor(std::lerp(0, max, Float())); }
+        static int Int(int min, int max) { return (int)std::floor(std::lerp(min, max, Float())); }
+        
+    private:
+        static std::mt19937 s_RandomEngine;
+        static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
+    };
+}
